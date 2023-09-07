577fce5afc8334cced2d5c59ae04e808a566c163
Minh Quan Dang <minh.quan040501@gmail.com>
2023-09-07 19:36:57 +0700

    init

---
+++ BusinessObjects/MyDBContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfiguration configuration = builder.Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("MyStoreDB"));
+        }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder optionsBuilder)
+        {
+            optionsBuilder.Entity<Category>().HasData(
+                new Category { CategoryID = 1, CategoryName = "Beverages" },
+                new Category { CategoryID = 2, CategoryName = "Condiments" },
+                new Category { CategoryID = 3, CategoryName = "Confections" },
+                new Category { CategoryID = 4, CategoryName = "Dairy Products" },
+                new Category { CategoryID = 5, CategoryName = "Grains/Cereals" },
+                new Category { CategoryID = 6, CategoryName = "Meat/Poultry" },
+                new Category { CategoryID = 7, CategoryName = "Produce" },
+                new Category { CategoryID = 8, CategoryName = "Seafood" }
+            );
+        }
+    }
+}
